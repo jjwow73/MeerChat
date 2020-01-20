833f6ef4d904dc71f6ff94a2a123fd2e846ac900
wkd3475 <wkd3475@gmail.com>
2020-01-20 19:05:54 +0900

    fix: 종료가 안되는 버그 수정

--- cmd/node/node.go
+++ cmd/node/node.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+
 	"github.com/wkd3475/MeerChat/pkg/client"
 	"github.com/wkd3475/MeerChat/pkg/server"
 )
@@ -17,19 +18,24 @@ func main() {
 
 	for {
 		var input int
+		checker := false
 		fmt.Println("1. set server")
 		fmt.Println("0. exit")
 		fmt.Scanf("%d", &input)
 
 		switch input {
 		case 0:
-			break
+			checker = true
 		case 1:
 			fmt.Printf("tartget server : ")
 			fmt.Scanf("%s", &targetAddress)
 			client.StartClient(targetAddress)
 		default:
 			fmt.Println("wrong input")
+		}
+
+		if checker {
+			break
 		}
 	}
 }
