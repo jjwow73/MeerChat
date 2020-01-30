2cb8ae7663bd776ff9fec549ad2fd81b6c36928c
wkd3475 <wkd3475@gmail.com>
2020-01-30 11:30:01 +0900

    refactor : 보기 좋게 수정

--- pkg/User/user.go
+++ pkg/User/user.go
@@ -1,7 +1,40 @@
-package User
+package user
+
+import (
+	"fmt"
+	"github.com/wkd3475/MeerChat/conf"
+	"github.com/wkd3475/MeerChat/pkg/util"
+)
 
 type User struct {
-	name string
-	ipAddress string
+	name       string
+	ipAddress  string
 	portNumber string
+}
+
+func NewUser() *User {
+	_ip := util.GetInboundIP()
+	conf.Ip = _ip
+	return &User{name: conf.Nickname, ipAddress: _ip, portNumber: conf.Port}
+}
+
+type UserError struct {
+	OriginalError error
+}
+
+func (userErr *UserError) Error() string {
+	return fmt.Sprintf("User error : %v", userErr.OriginalError)
+}
+
+func (user *User) GetUserInfo() (string, string, string) {
+	return user.name, user.ipAddress, user.portNumber
+}
+
+func (user *User) ModifyNickname(nickname string) error {
+	if len(nickname) < 0 || len(nickname) > 8 {
+		err := fmt.Errorf("nickname length -> 0~8")
+		return &UserError{OriginalError: err}
+	}
+	user.name = nickname
+	return nil
 }
--- pkg/room/room.go
+++ pkg/room/room.go
@@ -1,20 +1,20 @@
 package room
 
 import (
-	"../User"
+	"github.com/wkd3475/MeerChat/pkg/user"
 )
 
 type Room struct {
-	name string
-	admin User.User
-	password string
-	description string
-	ipAddress string
-	portNumber string
+	name          string
+	admin         user.User
+	password      string
+	description   string
+	ipAddress     string
+	portNumber    string
 	maxUserNumber int32
-	userList []User.User
-	blackList []User.User
-	updatedAt int32
+	userList      []user.User
+	blackList     []user.User
+	updatedAt     int32
 }
 
 func NewRoom(_name string, _description string, _password string, _ipAddress string, _portNumber string) *Room {
