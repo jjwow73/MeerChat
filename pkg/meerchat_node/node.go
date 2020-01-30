f2c0a5c14e173267c3cdfca9e3a403d49b98e288
wkd3475 <wkd3475@gmail.com>
2020-01-30 11:32:47 +0900

    Add : command receiver 추가

--- pkg/meerchat_node/node.go
+++ pkg/meerchat_node/node.go
@@ -17,3 +17,21 @@ func NewNode() *Node {
 	return &Node{User: _user}
 }
 
+func (node *Node) CommandReceiver(cuiChan chan protocol.Command) {
+	for {
+		select {
+		case v := <-cuiChan:
+			if v.CommandType == protocol.GetUserInfoCode {
+				name, ip, port := node.User.GetUserInfo()
+				answer := new(bytes.Buffer)
+				answer.WriteString(name)
+				answer.WriteString(ip)
+				answer.WriteString(port)
+				v.Answer = *answer
+				cuiChan <- v
+			} else if v.CommandType == protocol.ModifyNicknameCode {
+
+			}
+		}
+	}
+}
