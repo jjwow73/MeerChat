dbb8e5a8a2aee02e607ce94a7a6108d79284aeb5
wkd3475 <wkd3475@gmail.com>
2020-02-29 17:09:56 +0900

    feat: room의 생성자 구현

--- pkg/client/model/room.go
+++ pkg/client/model/room.go
@@ -1,15 +1,14 @@
 package model
 
-import "github.com/gorilla/websocket"
-
 type Room struct {
-	id string
+	id       string
 	password string
-	ip string
-	port string
-	conn *websocket.Conn
+	ip       string
+	port     string
 }
 
-func (room *Room) createRoom() {
+func NewRoom(id, password, ip, port string) (*Room, error) {
+	// TODO: input 검증
 
+	return &Room{id: id, password: password, ip: ip, port: port}, nil
 }
